3fbc216579a7e670844b0dd23f20bb3c00de2a8d
Ben Smith <ben@barxsmith.sbs>
2025-08-24 18:51:09 +0100

    1

--- src/App.tsx
+++ src/App.tsx
@@ -18,34 +18,57 @@ function App() {
     start: "flex",
   });
 
+  function gridCard(title: string, image: string, att?: string,w?:string) {
+    return (
+      <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 6, lg: 3 }}>
+        <Card shadow="md" style={{ aspectRatio: "10/11" }}>
+          <Card.Section>
+            <Center style={{ aspectRatio: "16/12" }}>
+              <Image title={att || undefined} src={image} w={w || "50%"} />
+            </Center>
+          </Card.Section>
+          <Center h="100%">
+            <Title order={4}>{title}</Title>
+          </Center>
+        </Card>
+      </Grid.Col>
+    );
+  }
+
   return (
     <MantineProvider>
-      <Center id="startpage" h="100vh" display={screen["start"]}>
+      {window.innerWidth < 575 ? <Space h="65vh"/> :""}
+      <Center id="startpage" h="100vh" w="100vw" display={screen["start"]}  mah="100vh">
+        
         <Stack maw="90vw">
-          <Title order={1} unselectable="off" style={{ textAlign: "center" }}>
+          <Title unselectable="off" style={{ textAlign: "center",fontSize:"3.2em" }}>
             What <b>X</b> For Me
           </Title>
           <Space h="md" />
           <Grid w="70vw">
-            <Grid.Col span={{ base: 2, xs: 12, sm: 6, md: 4, lg: 3 }}>
-              <Card shadow="md" style={{ aspectRatio: "10/11" }}>
-                <Card.Section>
-                  <Center style={{ aspectRatio: "16/12" }}>
-                    <Image
-                      title="Eleanor Chin, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
-                      src="https://upload.wikimedia.org/wikipedia/commons/e/e6/S25_Blueblack.png"
-                      w="50%"
-                    />
-                  </Center>
-                </Card.Section>
-                <Center h="100%">
-                  <Title order={4}>Phone</Title>
-                </Center>
-              </Card>
-            </Grid.Col>
-            <Grid.Col span={{ base: 2, xs: 12, sm: 6, md: 4, lg: 3 }}>
-              <Card shadow="md"></Card>
-            </Grid.Col>
+            {gridCard(
+              "Phone",
+              "https://upload.wikimedia.org/wikipedia/commons/e/e6/S25_Blueblack.png",
+              "Eleanor Chin, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
+            )}
+            {gridCard(
+              "Laptop",
+              "https://upload.wikimedia.org/wikipedia/commons/4/49/MacBook_Pro_Retina_001.jpg",
+              "SimonWaldherr, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons",
+              "80%"
+            )}
+            {gridCard(
+              "Headphones",
+              "https://upload.wikimedia.org/wikipedia/commons/4/4e/Wireless_3.0_Earbuds_Transparent_3.png",
+              "Trent Huss, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons",
+              "90%"
+            )}
+            {gridCard(
+              "Headphones",
+              "https://upload.wikimedia.org/wikipedia/commons/4/4e/Wireless_3.0_Earbuds_Transparent_3.png",
+              "Trent Huss, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons",
+              "90%"
+            )}
           </Grid>
         </Stack>
       </Center>
